ba1143fe0dd7b2b94bf8bce96d197797233389a5
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-09 19:36:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(arrayTesting){
+let winning = (arrayTesting) => arrayTesting.result === 'W';
+let ushindi =  arrayTesting.find(winning)  
+if (ushindi)
+{
+    return ushindi.year
+}
+else {
+    return undefined;
+}}
